8be58aed84a3dfbf845a9bef5f87013e57c24a1d
GEINOTECH <geinotech@gmail.com>
2025-08-28 15:28:43 +0100

    Started implementing homepage.dart

--- lib/OSs/MainPages/homepage.dart
+++ lib/OSs/MainPages/homepage.dart
@@ -1,1 +1,19 @@
-// Not yet implemented
+import 'package:flutter/material.dart';
+
+class Homepage extends StatelessWidget {
+  const Homepage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
+
+Widget _Activitymp({
+  required IconData icon,
+  required String label,
+  required String wolabel,
+  required String time,
+}) {
+  return Container();
+}
